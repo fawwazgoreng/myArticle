ad7e626c3e933496c5cca5d2c94e0b99ec8b45c4
fawwazgoreng <hatorimikael@gmail.com>
2026-06-01 11:49:20 +0700

    fix: dependency conflic, force do decrease vite version

---
+++ frontend/app/pages/login.vue
@@ -0,0 +1,9 @@
+<script>
+import { createAuthClient } from "better-auth/client";
+</script>
+
+<template>
+<div class="w-1/2 min-w-56 max-w-96 p-5 rounded-xl">
+    <span class="block w-full h-2 bg-red-"></span>
+</div>
+</template>
